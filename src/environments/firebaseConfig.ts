1d56deebc2cf2d030ef1d91d99fdd8f1a571d2d6
Praveena Kumar D G <praveenkumardg23@gmail.com>
2019-06-30 19:08:08 +0530

    APp

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyB46CxPDCXmI2xZjhv0YOABOLtDtH2oFeA",
+  authDomain: "tag-manager-2f6c4.firebaseapp.com",
+  databaseURL: "https://tag-manager-2f6c4.firebaseio.com",
+  projectId: "tag-manager-2f6c4",
+  storageBucket: "tag-manager-2f6c4.appspot.com",
+  messagingSenderId: "250533601886",
+  appId: "1:250533601886:web:c0b0e3e9624922e5"
+};
