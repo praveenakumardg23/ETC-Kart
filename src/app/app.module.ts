903f89ea1a0c8bcf8f1f9ed85f4822071d0fb517
Shreedhar N <Shreedhar415@gmail.com>
2019-07-14 14:20:23 +0530

    directive added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductModule } from './layouts/product/product.module';
 import { UserModule } from './layouts/user/user.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { GoogleAnalyticsDirective } from 'src/app/GoogleAnalyticsDirective';
 
 /* to load and set en.json as the default application language */
 export function setupTranslateFactory(service: TranslateService): Function {
@@ -18,7 +19,7 @@ export function setupTranslateFactory(service: TranslateService): Function {
 }
 
 @NgModule({
-	declarations: [ AppComponent ],
+	declarations: [ AppComponent, GoogleAnalyticsDirective],
 	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
